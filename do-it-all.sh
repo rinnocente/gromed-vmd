64b2a108aae1f8aff84d2241a1025ad9b32e040b
rinnocente <rinnocente@users.noreply.github.com>
2017-05-09 10:59:52 +0200

    Update do-it-all.sh

--- do-it-all.sh
+++ do-it-all.sh
@@ -5,7 +5,7 @@
 # the vmd-1.9.3.bin.LINUXAMD64-CUDA8-OptiX4-OSPRay111p1.opengl.tar.gz tarball in
 # the standard ~/Downloads/ directory.
 docker build -t gromed-vmd .
-CONT=`docker run -itd gromed-vmd`
+CONT=`docker run -P -itd gromed-vmd`
 PORT=`docker port $CONT|sed -e 's/.*://'`
 # initial passwd is mammamia
 ssh -X -p $PORT gromed@127.0.0.1
