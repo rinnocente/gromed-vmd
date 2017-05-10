6c14700508273412ad86f1c8d720b4a7c9d31c59
rinnocente <rinnocente@users.noreply.github.com>
2017-05-10 11:55:43 +0200

    Update do-it-all.sh

--- do-it-all.sh
+++ do-it-all.sh
@@ -4,8 +4,15 @@
 # we assume the user downloaded from http://www.ks.uiuc.edu/Development/Download/download.cgi
 # the vmd-1.9.3.bin.LINUXAMD64-CUDA8-OptiX4-OSPRay111p1.opengl.tar.gz tarball in
 # the standard ~/Downloads/ directory.
+#
 docker build -t gromed-vmd .
-CONT=`docker run -P -itd gromed-vmd`
-PORT=`docker port $CONT|sed -e 's/.*://'`
+read -p "Want to start the container now ? (y/n)" -i "n" ANS
+if [ "$ANS" = "y" ]
+then
+  CONT=`docker run -P -itd gromed-vmd`
+  PORT=`docker port $CONT|sed -e 's/.*://'`
 # initial passwd is mammamia
-ssh -X -p $PORT gromed@127.0.0.1
+  ssh -X -p $PORT gromed@127.0.0.1
+fi
+echo "When you want you can start the container with : (initial password is mammamia )"
+echo "  CONT=`docker run -P -itd gromed-vmd`;PORT=`docker port $CONT|sed -e 's/.*://'`;ssh -X -p $PORT gromed@127.0.0.1 
